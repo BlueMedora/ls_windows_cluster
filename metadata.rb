cc7d509d0defa4fa9c66d74de3a72653ab7886d0
John Snow <John Snow>
2016-03-23 19:59:24 -0400

    Create chef cookbook for managing windows failover clusters

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name 'ls_windows_cluster'
+maintainer 'The Authors'
+maintainer_email 'you@example.com'
+license 'all_rights'
+description 'Installs/Configures ls_windows_cluster'
+long_description 'Installs/Configures ls_windows_cluster'
+version '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,5 @@
+#
+# Cookbook Name:: ls_windows_cluster
+# Recipe:: default
+#
+# Copyright (c) 2016 The Authors, All Rights Reserved.
