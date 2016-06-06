f71fa216374c52953fc6e23b7b3ef9e8274c3863
John Snow <thelunaticscripter@outlook.com>
2016-06-06 16:21:08 -0400

    Update metadata with correct version.

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'you@example.com'
 license 'all_rights'
 description 'Installs/Configures ls_windows_cluster'
 long_description 'Installs/Configures ls_windows_cluster'
+version '0.2.1'
 
 depends 'windows'
-
